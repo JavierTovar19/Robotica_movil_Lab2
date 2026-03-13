1548a543be89ab4486813d5fba306e88c971c3fa
JavierTovar19 <datovarro@unal.edu.co>
2026-03-12 23:16:26 -0500

    Mejora de colisiones

--- turtu/scripts/otro_mas.py
+++ turtu/scripts/otro_mas.py
@@ -1,6 +1,5 @@
 #!/usr/bin/env python3.8
 
-
 import rospy 
 from geometry_msgs.msg import Twist 
 from random import random 
@@ -9,9 +8,9 @@ import numpy as np
 
 radio_muro=1
 limites=[0,11]
-velocidad_l=5
-velocidad_w=7
-frec=30
+velocidad_l=7
+velocidad_w=10
+frec=60
 
 X=0
 Y=0
@@ -26,27 +25,37 @@ def callback(data):
     Z = data.theta
 
 def sentido(X,Y,Z):
-    if X>limites[1]-radio_muro:
-        if Z>0:
+    Z2=Z+np.pi
+    if X>limites[1]-radio_muro:#MURO DERECHO
+        if np.pi/2<Z2<=np.pi:
+            return -velocidad_w
+        elif np.pi<Z2<3*np.pi/2:
             return velocidad_w
         else:
+            return 0
+    elif X<limites[0]+radio_muro:#MURO IZQUIERDO
+        if 3*np.pi/2<Z2<=2*np.pi:
             return -velocidad_w
-    elif X<limites[0]+radio_muro:
-        if Z<np.pi and Z>0:
-            return -velocidad_w
-        else:
-            return velocidad_w
-    elif Y>limites[1]-radio_muro:
-        if Z<np.pi/2:
-            return -velocidad_w
-        else:
-            return velocidad_w
-    elif Y<limites[0]+radio_muro:
-        if Z>-np.pi/2:
+        elif 0<Z2<np.pi/2:
             return velocidad_w
         else:
+            return 0
+    elif Y>limites[1]-radio_muro:#MURO SUPERIOR
+        if 3*np.pi/2<Z2<2*np.pi:
+            return velocidad_w
+        elif np.pi<Z2<=3*np.pi/2:
             return -velocidad_w
-        
+        else:
+            return 0
+    elif Y<limites[0]+radio_muro:#MURO INFERIOR
+        if 0<Z2<=np.pi/2:
+            return -velocidad_w
+        elif np.pi/2<Z2<np.pi:
+            return velocidad_w
+        else:
+            return 0
+    else:
+        return 0
 
 
 if __name__ == '__main__': 
